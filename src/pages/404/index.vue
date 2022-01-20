2942b8e31dbc0dba215bf1098f56faf4da4b5790
Cian Lu <lushan123888@gmail.com>
2022-01-20 18:56:20 +0800

    1.[添加]404页面

--- src/pages/404/index.vue
+++ src/pages/404/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    404
-  </div>
+  <v-container>
+    <app-bar/>
+    <v-row class="mt-16" justify="center">
+    </v-row>
+    <v-row justify="center">
+    </v-row>
+  </v-container>
 </template>
-
 <script>
+import AppBar from "@/pages/Admin/components/AppBar";
+
 export default {
   name: "404",
+  components: {
+    AppBar
+  },
   computed: {
     isMobile: function () {
       return this.$vuetify.breakpoint.mobile;
@@ -14,6 +22,9 @@ export default {
     isDark: function () {
       return this.$vuetify.theme.dark;
     },
+    lightPrimary: function () {
+      return this.$vuetify.theme.themes.light.primary;
+    }
   },
   data: function () {
     return {
