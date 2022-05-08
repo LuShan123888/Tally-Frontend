138adc49a8803a9f53a5729332c34add737ed5e7
Cian Lu <lushan123888@gmail.com>
2022-05-08 17:14:46 +0800

    1.[优化]异常拦截提示

--- src/axios/index.js
+++ src/axios/index.js
@@ -23,60 +23,51 @@ axios.interceptors.request.use((config) => {
 })
 
 // 响应拦截器
-axios.interceptors.response.use(
-    response => {
-        // console.log("==========response==========");
-        // console.log(response);
-        // console.log("==========end==========");
-        let code = response.data.code;
-        let message = response.data.message;
-        let data = response.data.data;
-        if (code && code !== 200) {
-            Element.Notification({
-                title: message,
-                message: data,
-                type: "error",
-                duration: 2000,
-            });
-        }
-        return response;
-    },
-    error => {
-        // console.log("==========response==========")
-        // console.log(error)
-        // console.log("==========end==========")
-        if (error.response === undefined) {
-            Element.Notification({
-                title: "服务器无响应",
-                message: null,
-                type: "error",
-                duration: 2000,
+axios.interceptors.response.use(response => {
+    // console.log("==========response==========");
+    // console.log(response);
+    // console.log("==========end==========");
+    let code = response.data.code;
+    let message = response.data.message;
+    let data = response.data.data;
+    if (code && code !== 200) {
+        Element.Notification({
+            title: message, message: data, type: "error", duration: 2000,
+        });
+    }
+    return response;
+}, error => {
+    // console.log("==========response==========")
+    // console.log(error)
+    // console.log("==========end==========")
+    if (error.response === undefined) {
+        Element.Notification({
+            title: "服务器无响应", message: null, type: "error", duration: 2000,
+        });
+        return Promise.reject(error.response);
+    }
+    let status = error.response.status;
+    let message = error.response.data.message;
+    let data = error.response.data.data ? error.response.data.data : error.response.data;
+    if (data instanceof Array) {
+        data = data.join('，');
+    }
+    Element.Notification({
+        title: message, message: data, type: "error", duration: 2000,
+    });
+    switch (status) {
+        case 401:
+            router.push({name: "SignIn"}).then(() => {
+                store.commit("clean");
             });
             return Promise.reject(error.response);
-        }
-        let status = error.response.status;
-        let message = error.response.data.message;
-        let data = error.response.data.data ? error.response.data.data : error.response.data;
-        Element.Notification({
-            title: message,
-            message: data,
-            type: "error",
-            duration: 2000,
-        });
-        switch (status) {
-            case 401:
-                router.push({name: "SignIn"}).then(() => {
-                    store.commit("clean");
-                });
-                return Promise.reject(error.response);
-            case 403:
-                return Promise.reject(error.response);
-            case 404:
-                return Promise.reject(error.response);
-            case 500:
-                return Promise.reject(error.response);
-            default:
-                return Promise.reject(error.response);
-        }
+        case 403:
+            return Promise.reject(error.response);
+        case 404:
+            return Promise.reject(error.response);
+        case 500:
+            return Promise.reject(error.response);
+        default:
+            return Promise.reject(error.response);
     }
-)
+})
